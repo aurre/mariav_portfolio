2a0b25b8d7ff0a3fe0c2acc356f074b3400f8a07
Raysa Aurrecochea <aurrecochearaysa@gmail.com>
2018-08-05 16:58:32 -0500

    change password

--- email.js
+++ email.js
@@ -1,10 +1,11 @@
 const nodemailer = require('nodemailer');
 
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'frontdesk.chicago17',
-    pass: 'aurrecochea@92',
+    pass: 'mAprOs18',
   },
 });
 
