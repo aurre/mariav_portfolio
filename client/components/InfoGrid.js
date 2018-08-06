7e0a278e55006ca7d6e86f8722b3f41582c67383
Raysa Aurrecochea <aurrecochearaysa@gmail.com>
2018-08-06 11:33:31 -0500

    change style for portfolio view

--- client/components/InfoGrid.js
+++ client/components/InfoGrid.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import {Grid, Card, CardContent, Typography} from '@material-ui/core';
 
 import PortfolioItems from './PortfolioItems';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
+    display: 'flex',
+    justifyContent: 'space-evenly'
   },
   demo: {
     backgroundColor: theme.palette.background.paper,
   },
-  title: {
-    margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`,
-  },
+
 });
 
-class InteractiveList extends React.Component {
+class InfoGrid extends React.Component {
 
   render() {
     const { classes } = this.props;
@@ -25,27 +25,29 @@ class InteractiveList extends React.Component {
 
     const values1 = ['Disfunciones sexuales', 'Migraña', 'Colon irritado', 'Alergias', 'Gastritis', 'Bulimia', 'Anorexia', 'Asfixia','Bloqueos', 'Enuresis', 'Adicciones', 'Dolores y enfermedades inexplicables']
 
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={24}>
+    return <div className={classes.root}>
+        <Card style={{height: '510px'}}>
+          <Typography variant="headline" component="h6">
+            Enfermedades emocionales como:
+          </Typography>
           {values.map(item => {
-            return <PortfolioItems key={item} props={item} />
+            return <PortfolioItems key={item} props={item} />;
           })}
-        </Grid>
-        <br/>
-        <p>Enfermedades Psicosomáticas:</p>
-        <Grid container spacing={24}>
+        </Card>
+        <Card style={{height: '510px'}}>
+          <Typography variant="headline" component="h6">
+            Enfermedades Psicosomáticas:
+          </Typography>
           {values1.map(item => {
-            return <PortfolioItems key={item} props={item} />
+            return <PortfolioItems key={item} props={item} />;
           })}
-        </Grid>
-      </div>
-    );
+        </Card>
+      </div>;
   }
 }
 
-InteractiveList.propTypes = {
+InfoGrid.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(InteractiveList);
+export default withStyles(styles)(InfoGrid);
--- client/components/PorfolioInfo.js
+++ client/components/PorfolioInfo.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import CardContent from '@material-ui/core/CardContent';
-import InfoGrid from './InfoGrid'
+import InfoGrid from './InfoGrid';
+import  Typography  from '@material-ui/core/Typography';
 
 const PorfolioInfo = () => {
   return (
-    <CardContent>
-      <h6>¿Qué se puede tratar con Hipnosis?</h6>
-      <p>Se pueden tratar mediante la Hipnosis enfermedades emocionales como:</p>
-
+    <div>
+      <Typography gutterBottom variant="headline" component="h1">
+        ¿Qué se puede tratar con Hipnosis?
+      </Typography>
       <InfoGrid />
-    </CardContent>
+    </div>
   )
 }
 
