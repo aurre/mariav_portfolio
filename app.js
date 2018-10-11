8e4bda7baca3fefaf861b0de4ab22231183c222e
raysa <aurrecochearaysa@gmail.com>
2018-10-11 17:28:56 -0500

    change videos link to youtube

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static('.'));
 
+app.post('/', (req, res, next) => {
+  console.log(req);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
